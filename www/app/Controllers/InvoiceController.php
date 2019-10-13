6defbd9badf1f0bf180c394db074cfa258c61018
Samuel Bretas <samuelbretas@gmail.com>
2019-10-13 17:02:10 -0300

    criacao Classe controlle invoice direcionando metodos list e print

---
+++ www/app/Controllers/InvoiceController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Helpers\InputHelper as Input;
+
+class InvoiceController extends BaseController
+{
+    private $invoice;
+    private $list;
+    private $objectMap;
+
+
+    public function list()
+    {
+
+    }
+
+    public function print()
+    {
+
+    }
+
+    
+}
